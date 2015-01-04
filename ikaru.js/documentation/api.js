49d051c090db05d0b5798ac67e6dff614d49abbb
Roy Surjano <rsurjano@outlook.com>
2015-01-03 20:35:52 -0500

    added ikaru.js 75%

---
+++ ikaru.js/documentation/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "COORDINATES",
+        "CSS",
+        "DOM",
+        "EVENTS",
+        "HTML",
+        "LIST",
+        "TRANSVERSAL"
+    ],
+    "modules": [
+        "ikaru.js"
+    ],
+    "allModules": [
+        {
+            "displayName": "ikaru.js",
+            "name": "ikaru.js",
+            "description": "ikaru.js is a lightweiht & Faster DOM manipulation cross Browser"
+        }
+    ]
+} };
+});
